ebcbbfea0a9edbd501a5b446c37d638eeab4ee50
Aruuni <mihaifcp44279@gmail.com>
2023-10-16 18:10:34 +0100

    added builder

---
+++ ns3-setup.sh
@@ -0,0 +1,8 @@
+echo "cloning ns3"
+git clone https://gitlab.com/nsnam/ns-3-dev.git
+cp SimulatorScript.cc ns-3-dev/scratch/
+cd ns-3-dev
+./ns3 configure --build-profile=optimized 
+echo "Building ns3"
+./ns3
+echo "DONE"
